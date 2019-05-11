ba894d003d17ea5bd504aaaea5b91620fdf8d3d1
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2019-05-11 21:51:05 +0200

    Add preferences support
    With redux preferences and sharedpreferences.

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,59 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
 import 'splashscreen.dart';
-import 'tabs/counter.dart';
-import 'tabs/myobs.dart';
+import 'state.dart';
 
+import 'package:redux/redux.dart';
+import 'package:redux_persist/redux_persist.dart';
+import 'package:redux_persist_flutter/redux_persist_flutter.dart';
 
-void main() => runApp(new MaterialApp(
-      home: new SplashScreen(),
-      routes: <String, WidgetBuilder>{
-        '/homeScreen': (BuildContext) => new VigiloApp()
-      },
-    ));
+import 'VigiloApp.dart';
 
-class VigiloApp extends StatelessWidget {
-  
-  @override
-  Widget build(BuildContext context) {
-    ThemeData theme = ThemeData(
-      primarySwatch: Colors.yellow,
-    );
-    return MaterialApp(
-        title: 'Vigilo',
-        theme: theme,
-        home: DefaultTabController(
-          length: 3,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Row(
-                children: <Widget>[
-                  Image.asset("assets/images/jeSuisUnDesDeux.png", width: 32),
-                  SizedBox(width: 16),
-                  Text(
-                    "Montpellier",
-                  ),
-                ],
-              ),
-              bottom: TabBar(isScrollable: true, tabs: <Widget>[
-                Tab(
-                  text: "Bicycle",
-                  icon: Icon(Icons.directions_bike),
-                ),
-                Tab(
-                  text: "Observation",
-                  icon: Icon(Icons.local_see),
-                ),
-                Tab(
-                  text: "Counter",
-                  icon: Icon(Icons.plus_one),
-                )
-              ]),
-            ),
-            body: TabBarView(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Text("Bike"),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: ObservationWidget(theme),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: CounterPage(title: "Montpellier"),
-                )
-              ],
-            )
-          ),
-        ));
+void main() async {
+  final persistor = Persistor<AppState>(
+    storage: FlutterStorage(),
+    serializer: JsonSerializer<AppState>(AppState.fromJson),
+  );
+
+  // Load initial state
+  final initialState = await persistor.load();
+
+  final store = Store<AppState>(
+    reducer,
+    initialState: initialState ?? AppState(),
+    middleware: [persistor.createMiddleware()],
+  );
+
+  runApp(new MaterialApp(
+    home: new SplashScreen(),
+    routes: <String, WidgetBuilder>{
+      '/homeScreen': (BuildContext) => new VigiloApp(store)
+    },
+  ));
+}
+
+AppState reducer(AppState state, Object o) {
+  if (o is CitySelected) return state.copyWith(city: o.city);
+
+  if (o is Count) {
+    Map<String, int> counters = Map.from(state.counters);
+    switch (o.action) {
+      case CounterActions.Increment:
+        counters.update(o.name, (v) => v + 1, ifAbsent: () => 1);
+        break;
+      case CounterActions.Decrement:
+        counters.update(o.name, (v) => max(0, v - 1), ifAbsent: () => 0);
+        break;
+      case CounterActions.Reset:
+        if (o.name == "") {
+          counters.updateAll((k, v) => 0);
+        } else
+          counters.update(o.name, (v) => 0, ifAbsent: () => 0);
+        break;
+    }
+    return state.copyWith(counters: counters);
   }
+  return state;
 }
---
+++ lib/preferences.dart
@@ -0,0 +1,13 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'dart:async';
+
+class Preferences {
+
+
+  static Future<String> getCity() async {
+    var pref = await SharedPreferences.getInstance();
+    return pref.getString("city") ?? "Montpellier";
+  }
+
+}
